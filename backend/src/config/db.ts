66c2d7cce83eada6c03e5cf9ffe23663c0071422
HardUsername-123 <anthonycrausus14.darkcode@gmail.com>
2026-01-03 17:48:42 +0800

    chore: update Docker configuration for development, set environment variables, and adjust Prisma client setup

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,14 +1,13 @@
 import { PrismaClient } from "../generated/prisma/client.js";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["query", "error", "warn"],
-  } as any);
+    accelerateUrl: process.env.DATABASE_URL!,
+    log: ["query", "warn", "error"],
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
