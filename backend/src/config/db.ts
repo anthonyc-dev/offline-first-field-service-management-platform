510d96aaf1afab7a5bb316770e0c8adc5342f983
HardUsername-123 <anthonycrausus14.darkcode@gmail.com>
2026-01-03 22:12:32 +0800

    chore: update Docker configuration for development, remove production files, and add PostgreSQL adapter for Prisma

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,14 +1,10 @@
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client.js";
 
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+const connectionString = `${process.env.DATABASE_URL}`;
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    accelerateUrl: process.env.DATABASE_URL!,
-    log: ["query", "warn", "error"],
-  });
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+export { prisma };
